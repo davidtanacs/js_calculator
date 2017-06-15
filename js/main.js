91fe6bc75fc391e2d00fef13ccfdf60198b23dc6
davidtanacs <tanacsdavid90@gmail.com>
2017-06-15 14:13:27 +0200

    % works as percentage, not as modulo.

--- js/main.js
+++ js/main.js
@@ -1,43 +1,61 @@
-var current = "0";
-maxLength = 30;
+var current = "";
 
 function addDigit(digit) {
-    if (current.length > maxLength)
+    if (current.length > 30)
     { current = 'Sorry, it\'s too long'}
     else {
-        try {
-            if ((eval(current) == 0) && (current.indexOf(".") == -1))
-            {current = digit}
-            else
-            { current = current + digit} }
-        catch(err) {
-            current = current + digit
+        if (current == "" || current == '0' && current.search('.') == false) {
+            current = digit;
         }
-    
-    } 
+        else {
+            current = current + digit;
+        } } 
     document.calculator.display.value = current;
 }
 
 function addDecimal() {
-    if (current[current.length - 1] == '.')
-    { current = current}
-    else
-    {current = current + '.';}
+    if (current[current.length - 1] == '.'){
+        current = current;
+    }
+    else{
+        current = current + '.';
+    }
     document.calculator.display.value = current;
 }
 
 
 function clearDisplay() {
     document.calculator.display.value = "";
-    current = "0"
+    current = "0";
 }
 
 function addOperation(operation) {
-    current = current + operation 
-    document.calculator.display.value = current
+    current = current + operation;
+    document.calculator.display.value = current;
 }
 
 function executeOperation() {
-    current = eval(current)
-    document.calculator.display.value = current
+    if (current.includes('%')) {
+        current = calculatePercentage();
+    }
+    else {
+        current = eval(current);
+    }
+    document.calculator.display.value = current;
+}
+
+function calculatePercentage() {
+    var base = ""
+    var percentage = ""
+    var position = current.search('%')
+    for (var i = 0; i < current.length; i++) {
+        if (i < position) {
+            base = base + current[i];
+        }
+        else if (i > position) {
+            percentage = percentage + current[i];
+        }
+    }
+    current = (eval(base) / 100) * eval(percentage);
+    return current
 }
